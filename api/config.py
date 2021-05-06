d7c10dcc344c9f92d6edd740007da9cec9bf18a6
Alex Brady <alex.brady@blueface.com>
2021-05-06 16:59:11 +0100

    remove db, make path relative

--- api/config.py
+++ api/config.py
@@ -1,7 +1,10 @@
 
+import os
+
+db_path = os.path.join(os.path.dirname(__file__), 'user_tests.db')
+db_uri = 'sqlite:///{}'.format(db_path)
 
 
 class Base:
-    SQLALCHEMY_DATABASE_URI = 'sqlite:////home/alex/PycharmProjects/UserTests/oculid.db'
+    SQLALCHEMY_DATABASE_URI = db_uri
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-
